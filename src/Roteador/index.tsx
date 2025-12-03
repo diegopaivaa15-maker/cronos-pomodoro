f1002ba860fbc16ddf5dd97cb86774b030c12c0f
Diego Paiva <diegopaivaa15@gmail.com>
2025-12-03 19:55:10 -0300

    roteador

--- src/Roteador/index.tsx
+++ src/Roteador/index.tsx
@@ -1,19 +1,7 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { Home } from "../PAGES";
 import { NotFound } from "../NotFound";
 import { AboutPomodoro } from "../componentes/AboutPomodoro";
-import { useEffect } from "react";
-
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [pathname]);
-
-  return null;
-}
 
 export function Roteador(){
     return (
